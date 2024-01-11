2bdbaefbb4462d6aa777fb6cf81951a6a4672dc2
whisky <wk20238899@gmail.com>
2024-01-11 17:43:26 +0400

    first commit

---
+++ vue3-prettier-eslint/.eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off' // 默认vue文件必须大驼峰命名，off 关闭文件名称校验
+  }
+}
